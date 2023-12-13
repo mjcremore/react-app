f5e82574f86b033007ae21e4112dea0d951ad23c
mc <mccm1118@gmail.com>
2023-12-13 17:09:09 +0900

    attempt #4

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 import { Amplify } from 'aws-amplify';
 // import config from './aws-exports';
-import config from './aws-exports.js';
-
+import config from 'src/aws-exports.js';
 Amplify.configure(config);
 
 
